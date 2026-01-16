a69be51b0368c3f0c1c7ee096f8579612282a12b
68011101-suppalerk <68011101@kmitl.ac.th>
2026-01-16 11:16:05 +0700

    ll

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,15 +1,32 @@
-export interface safeFetchComment{
-  id : number ; 
-  body : string ; 
+export interface Comment  {
+  id : number ;
+  body : string;
 }
 
-export function safeFetchComment(id : number) {  
+async function safeFetchComment(commentId: number) {
+  try {
+    if (commentId <= 0) {
+      return null;
+    }
+
+    const feedback = await fetch("https://jsonplaceholder.typicode.com/comments/" + commentId);
+
+    if (feedback.ok === false) {
+      return null;
+    }
+    const comment = await feedback.json();
+
+    return {
+      id: comment.id,
+      body: comment.body
+    };
+  } catch {
+    return null;
+  }
 }
 
-safeFetchComment(1)
-console.log()
-
-
-
-
-
+console.log(safeFetchComment(1))
+console.log(safeFetchComment(9999))
+console.log(safeFetchComment(-1))
+//console.log(safeFetchComment(null))
+//console.log(safeFetchComment(undefined))
